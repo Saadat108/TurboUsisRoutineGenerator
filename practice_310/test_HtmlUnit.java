49b43687a108610adb3e10d3d93b36edc85be723
Saadat <s.saadathussain.95@gmail.com>
2019-03-24 01:58:38 +0600

    still not working

--- practice_310/test_HtmlUnit.java
+++ practice_310/test_HtmlUnit.java
@@ -10,6 +10,8 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
+import com.gargoylesoftware.htmlunit.html.HtmlOption;
 //import com.gargoylesoftware.htmlunit.html.*;
 
 /**
@@ -38,13 +40,20 @@ public class test_HtmlUnit {
             passwordInput.setValueAttribute(password);
 //            System.out.println(page.asText()); // to test whether name ans password have been entered
             page = page.getHtmlElementById("ctl00_leftColumn_ctl00_btnLogin").click(); // clicks on submit button
+            
+            
             // goes to find class timing
+            //method 1
+//            HtmlSelect select = (HtmlSelect) page.getElementById("accordion1");
+//            HtmlOption option = select.getOptionByValue("Student");
+//            select.setSelectedAttribute(option, true);
             
-            HtmlAnchor htmlAnchor = page.getAnchorByHref("#/academia/academicSection/showSectionForStudent"); 
-            HtmlPage page3 = htmlAnchor.click();
-            page3 = htmlAnchor.click();
+            // method 2
+//            HtmlAnchor anchor = page.getAnchorByName("Show Class Schedule");
+//            HtmlPage page3 = anchor.click();
+//            System.out.println(page3.asText());
             
-            System.out.println(page3.asText());
+            System.out.println(page.asText());
 //            System.out.println(page.asXml());
         } catch (Exception e) {
             e.printStackTrace();
